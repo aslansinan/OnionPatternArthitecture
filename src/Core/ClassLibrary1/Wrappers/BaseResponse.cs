17180f0548e261a946e4f89d3571910b60ada5c3
sinan <aslansinan586@gmail.com>
2022-11-29 13:17:43 +0300

    dosya içerik düzenleme

---
+++ src/Core/ClassLibrary1/Wrappers/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionPattern.Application.Wrappers
+{
+    public class BaseResponse
+    {
+        public Guid id { get; set; }
+        public bool  Success { get; set; }
+        public string message { get; set; }
+
+    }
+}
