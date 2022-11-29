b86d8e3e63c4bc4fb966a51aad0f9807b72c7a5f
sinan <aslansinan586@gmail.com>
2022-11-29 10:51:44 +0300

    infrastructers katmanı sonu

---
+++ src/infrastructure/OnionPattern.Persistences/Repository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnionPattern.Application.Interface.Repository;
+using OnionPattern.Persistences.Context;
+using OnionPatternDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionPattern.Persistences.Repository
+{
+    internal class ProductRepository :GenericRepository<Product>,IProductRepository
+    {
+
+        public ProductRepository(ApplicationDbContext dbContext):base(dbContext)
+        {
+        }
+    }
+}
